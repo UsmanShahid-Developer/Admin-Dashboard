bc783d308ec00ca2a49b2818078c0c00e9b7569f
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-10-08 10:42:41 -0700

    Full Dashboard

---
+++ app/(components)/SideBar.js
@@ -0,0 +1,114 @@
+'use client'
+import { forwardRef } from "react";
+import Link from "next/link";
+import { HomeIcon, CreditCardIcon, UserIcon } from "@heroicons/react/24/solid"; 
+import { useRouter } from "next/navigation";
+import {BiSolidDashboard} from "react-icons/bi"
+import {PiStudentFill} from "react-icons/pi"
+import {SiCoursera} from "react-icons/si"
+import {RiPresentationLine} from "react-icons/ri"
+
+const SideBar = forwardRef(({ showNav }, ref) => {
+  const router = useRouter();
+
+  return (
+    <div ref={ref} className="fixed w-56 h-full bg-white shadow-sm">
+      <div className="flex justify-center mt-6 mb-14">
+        <picture>
+          <img
+            className=" w-49 h-full bg-white  "
+            src="/ferox-transparent.jpg.jpg"
+            alt="company logo"
+          />
+        </picture>
+      </div>
+
+      <div className="flex flex-col">
+        <Link href="/">
+          <div
+            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+              router.pathname == "/"
+                ? "bg-orange-100 text-blue-500"
+                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+            }`}
+          >
+            <div className="mr-2">
+            <BiSolidDashboard/>
+            </div>
+            <div>
+              <p>Dashboard</p>
+            </div>
+          </div>
+        </Link>
+        <Link href="/students">
+          <div
+            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+              router.pathname == "/account"
+                ? "bg-blue-100 text-blue-500"
+                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+            }`}
+          >
+            <div className="mr-2">
+              <PiStudentFill/>
+            </div>
+            <div>
+              <p>Students</p>
+            </div>
+          </div>
+        </Link>
+        
+        <Link href="/courses">
+          <div
+            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+              router.pathname == "/account"
+                ? "bg-blue-100 text-blue-500"
+                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+            }`}
+          >
+            <div className="mr-2">
+              
+              <SiCoursera />
+            </div>
+            <div>
+              <p>Course</p>
+            </div>
+          </div>
+        </Link>
+        <Link href="/attendance">
+          <div
+            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+              router.pathname == "/account"
+                ? "bg-blue-100 text-blue-500"
+                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+            }`}
+          >
+            <div className="mr-2">
+              <RiPresentationLine />
+            </div>
+            <div>
+              <p>Attendanse</p>
+            </div>
+          </div>
+        </Link>
+        {/* <Link href="/accounts">
+          <div
+            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+              router.pathname == "/account"
+                ? "bg-blue-100 text-blue-500"
+                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+            }`}
+          >
+            
+            
+          </div> */}
+        {/* </Link> */}
+       
+       
+      </div>
+    </div>
+  );
+});
+
+SideBar.displayName = "SideBar";
+
+export default SideBar;
---
+++ app/(components)/headercard.js
@@ -0,0 +1,42 @@
+import {BsGraphUp} from "react-icons/bs"
+import {BsEmojiSmile} from "react-icons/bs"
+import {BsEmojiFrown} from "react-icons/bs"
+// import Barchart from "./Barchart/barchart"
+export default function Headercard() {
+  return (
+    <>
+
+    <p className="text-gray-700 text-3xl mb-16 font-bold">Dashboard</p>
+
+    <div className="grid lg:grid-cols-3 gap-5 mb-16">
+      <div className="rounded bg-white h-40 shadow-sm outline outline-offset-0 outline-red-400">
+        <h1 className="text-4xl	text-center mt-5	text-red-500"> Students</h1>
+        <h1 className="text-center mt-5 text-orange-500	 text-3xl">190.13k</h1>
+        <BsGraphUp className="text-orange-500 text-3xl mt-2 ms-32"/>
+      </div>
+      <div className="rounded bg-white outline outline-offset-0 outline-blue-400 h-40 shadow-sm">
+      <h1 className="text-4xl	text-center mt-5	text-blue-500"> All courses </h1>
+        <h1 className="text-center mt-5 text-orange-500	 text-3xl">49.4%</h1>
+        <BsEmojiFrown className="text-red-500 text-3xl mt-2 ms-32"/>
+      </div>
+      <div className="rounded bg-white h-40 outline outline-offset-0 outline-red-400 shadow-sm">
+      <h1 className="text-4xl	text-center mt-5	text-blue-500"> Present</h1>
+        <h1 className="text-center mt-5 text-orange-500	 text-3xl">78.9%</h1>
+        <BsEmojiSmile className="text-red-500 text-3xl mt-2 ms-32"/>   
+      </div>
+    </div>
+    <div className="grid col-1 bg-white h-96 shadow-sm">
+    <picture>
+          <img
+            className=" w-49 h-full bg-white w-100 "
+            src="/1.jpg"
+            alt="company logo"
+          />
+        </picture>
+    </div>
+    <h1>
+      All Rese
+    </h1>
+  </>
+  )
+}
